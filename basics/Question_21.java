e6d66189379cd8458d6493048a635bdde132af77
Mohit Mishra <mishramohit2510@gmail.com>
2023-06-21 19:59:35 +0530

    initial commit

--- basics/Question_21.java
+++ basics/Question_21.java
@@ -2,8 +2,8 @@ public class Question_21 {
 
     public static void main(String args[]){
 
-    int 12var = 10;
-    System.out.println(12var);
+    int String = 10;
+    System.out.println(String);
 
     }    
 }
